098861351bb3e486aee479028faf5e4c33db08ea
sayomais <sayomais@outlook.com>
2024-11-26 20:42:43 +0500

    Add files via upload

---
+++ all/proxis/array_proxies.py
@@ -0,0 +1,15 @@
+#PROXIS LIMPIAS CAMBIAR CUANDO DEJEN DE FUNCIONAR 
+
+proxies = [
+    'http://kqwvwfpv-rotate:2u52ir82n3l0@p.webshare.io:80',
+    'http://mwjfsqbt-rotate:plxg0wskkci1@p.webshare.io:80',
+    'http://thqocqri-rotate:debn99lhwlu4@p.webshare.io:80',
+    'http://psiihqvp-rotate:ixpxh7emxmx7@p.webshare.io:80',
+    'http://zaotcems-rotate:lpytvgdtqui3@p.webshare.io:80',
+    'http://kalgpsyn-rotate:vlfvpdfsy85o@p.webshare.io:80',
+    'http://abxzkfmg-rotate:u6ds96f1s1a1@p.webshare.io:80',
+    'http://tujetyem-rotate:rh627bedlo09@p.webshare.io:80',
+    'http://lodlyfpg-rotate:zba2ls0khups@p.webshare.io:80',
+    'http://ldlbqbpj-rotate:lreqnm214sev@p.webshare.io:80',
+    'http://rmwmzodn-rotate:dsn7b6e6aveg@p.webshare.io:80',
+]
